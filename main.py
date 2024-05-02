a34f3a8c25d8548125c168629df74d9d82f83a85
BananaChad <105674111+BananaChad@users.noreply.github.com>
2024-05-02 22:22:03 +0200

    start working on this project!

---
+++ main.py
@@ -0,0 +1,11 @@
+# ok how will i do this
+# Asetall is a auto installer for popular tool aseprite (https://www.github.com/aseprite/aseprite)
+# I am NOT affiliated with aseprite and this is not an official tool
+
+# this script expects you to save files like skia to their default paths, if the script cant find it it will prompt you to A: install for you or B: let you specify a new path
+
+
+# start by using git clone --recursive https://github.com/aseprite/aseprite.git if it cant find /aseprite
+# if it updates ase start by discarding all changes (ADD WARNING!) and git pull and update submodule
+
+#
